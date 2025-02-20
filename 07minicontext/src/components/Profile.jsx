97d7fd6085848039daabbfb04f1dfe730a6e1283
Hari Vasantham <narahariv94@gmail.com>
2025-02-20 11:30:03 +0530

    created global state using context api

---
+++ 07minicontext/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <h1>Not logged in</h1>;
+
+  return <h1>Profile : {user.username}</h1>;
+}
+export default Profile;
